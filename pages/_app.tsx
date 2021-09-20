591de4f4c087e839827f28a4a1a8c5f4e8e6b126
amroczeK <mroczek9990@gmail.com>
2021-09-20 21:02:48 +0800

    enter page and cleanup

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-
-import { GetServerSideProps } from 'next/app';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
