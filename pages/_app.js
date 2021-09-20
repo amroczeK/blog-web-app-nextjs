872974aadf0398fe9a43408424faf73fffc35220
amroczeK <mroczek9990@gmail.com>
2021-09-20 21:47:30 +0800

    firebase setup navbar and toaster

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
-import Navbar from '../components/Navbar'
-
+import Navbar from '../components/Navbar';
+import {Toaster} from 'react-hot-toast';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />
+      <Toaster/>
     </>
   );
 }
