9466c7f4201442c48ff76dbfdab1cd45c7c03ef3
amroczeK <mroczek9990@gmail.com>
2021-09-22 22:54:47 +0800

    custom username choice

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,5 @@
-import { auth, googleAuthProvider } from '../lib/firebase';
-import { useContext } from 'react';
-import { UserContext } from '../lib/context';
+import Enter from "./enter"
 
 export default function Home(props) {
-  const { user, username } = useContext(UserContext);
-
-  // 1. user signed out <SignInButton/>
-  // 2. user signed in, but missing username <UsernameForm/>
-  // 3. user signed in, has username <SignOutButton/>
-  return <main>{user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}</main>;
-}
-
-function SignInButton() {
-  const signInWithGoogle = async () => {
-    await auth.signInWithPopup(googleAuthProvider).catch(console.error);
-  };
-
-  return (
-    <button className='btn-google' onClick={signInWithGoogle}>
-      <img src={'/google.png'} /> Sign in with Google
-    </button>
-  );
-}
-
-function SignOutButton() {
-  return <button onClick={() => auth.signOut()}>Sign Out</button>;
-}
-
-function UsernameForm() {
-  return <></>;
+  return <Enter/>;
 }
