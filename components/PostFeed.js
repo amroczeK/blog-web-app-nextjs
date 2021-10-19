13b29e25f2fa32db890a10dd54300684bb4c3939
amroczeK <mroczek9990@gmail.com>
2021-10-19 11:18:47 +0800

    posts feed component update

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -17,20 +17,36 @@ function PostItem({ post, admin = false }) {
     <div className="card">
       <Link href={`/${post.username}`}>
         <a>
-          <strong>By @{post.username}</strong>
+          <strong>By @{post.username} </strong>
         </a>
       </Link>
       <Link href={`/${post.username}/${post.slug}`}>
-        <a>
-          <strong>{post.title}</strong>
-        </a>
+        <h2>
+          <a>{post.title}</a>
+        </h2>
       </Link>
       <footer>
         <span>
           {wordCount} words. {minutesToRead} min read
         </span>
-        <span>{post.heartCount} Hearts</span>
+        <span className="push-left">💗 {post.heartCount || 0} Hearts</span>
       </footer>
+      {/* If admin view, show extra controls for user */}
+      {admin && (
+        <>
+          <Link href={`/admin/${post.slug}`}>
+            <h3>
+              <button className="btn-blue">Edit</button>
+            </h3>
+          </Link>
+
+          {post.published ? (
+            <p className="text-success">Live</p>
+          ) : (
+            <p className="text-danger">Unpublished</p>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
