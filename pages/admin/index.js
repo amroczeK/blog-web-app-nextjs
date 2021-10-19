87ffb51fb10afe76efd84acf4742dd6e4fde9558
amroczeK <mroczek9990@gmail.com>
2021-10-19 14:44:10 +0800

    auth on routes

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,13 @@
 import Metatags from "../../components/Metatags"
+import AuthCheck from "../../components/AuthCheck";
 
 export default function AdminPostsPage({}) {
   return (
     <main>
-      <Metatags title="admin page"/>
-      <h1>Posts</h1>
+      <AuthCheck>
+        <Metatags title="admin page" />
+        <h1>Posts</h1>
+      </AuthCheck>
     </main>
   );
 }
