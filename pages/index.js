74ceb2cb46962979fc3b75352b37ec7062fd2974
amroczeK <mroczek9990@gmail.com>
2021-10-19 11:19:22 +0800

    home/feeds page update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
-import Enter from "./enter"
+import { useState } from "react";
+import PostFeed from "../components/PostFeed";
+import Loader from "../components/Loader";
+import {
+  getPostsCollection,
+  getMorePostsCollection,
+  postToJSON,
+  fromMillis,
+} from "../lib/helpers";
+
+const POSTS_LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postQuery = getPostsCollection(POSTS_LIMIT);
+
+  const posts = (await postQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
 
 export default function Home(props) {
-  return <div></div>
+  const [posts, setPosts] = useState(props.posts); // Use props rendered on server as initial value
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = getMorePostsCollection(POSTS_LIMIT, cursor);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < POSTS_LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+    <main>
+      <PostFeed posts={posts} />
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+      <Loader show={loading} />
+      {postsEnd && "You have reached the end!"}
+    </main>
+  );
 }
