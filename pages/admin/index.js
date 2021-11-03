8728a02a58e088dac87fb7702325a55f00ed74ad
amroczeK <mroczek9990@gmail.com>
2021-11-03 15:04:34 +0800

    admin page changes

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,103 @@
-import Metatags from "../../components/Metatags"
+import styles from "../../styles/Admin.module.css";
+import Metatags from "../../components/Metatags";
 import AuthCheck from "../../components/AuthCheck";
+import PostFeed from "../../components/PostFeed";
 
-export default function AdminPostsPage({}) {
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+import { UserContext } from "../../lib/context";
+import { firestore, auth } from "../../lib/firebase";
+import { getUserRef, serverTimestamp } from "../../lib/helpers";
+
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
+
+export default function AdminPostsPage(props) {
   return (
     <main>
       <AuthCheck>
         <Metatags title="admin page" />
         <h1>Posts</h1>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
+  );
+}
+
+function PostList() {
+  const ref = getUserRef();
+  const query = ref.orderBy("createdAt");
+  const [querySnapshot] = useCollection(query);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate length
+  const isValid = title.length > 3 && title.length < 100;
+
+  // Create a new post in firestore
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore
+      .collection("users")
+      .doc(uid)
+      .collection("posts")
+      .doc(slug);
+
+    // Give all fields a default value here
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# hello world!",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await ref.set(data);
+
+    toast.success("Post created!");
+
+    // Imperative navigation after doc is set
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Article!"
+        className={styles.input}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
   );
 }
